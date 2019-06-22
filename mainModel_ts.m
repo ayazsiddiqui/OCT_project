e22ae5bc2979b5c9bb7441db49c7b7a6a678095e
Ayaz <asiddiq2@ncsu.edu>
2019-06-21 22:20:15 -0400

    removed from each iterator from everywhere. drastically improved simulation speed. model runs and produces believable results -AS

--- mainModel_ts.m
+++ mainModel_ts.m
@@ -15,6 +15,12 @@ MI = [14330000 0 0; 0 143200 0; 0 0 15300000];
 vol = 111.7000;
 Rcb_cm = [0;0;0];
 
+class_liftingBdy(1).tetherAttchPt = [-2.5000; -20.0000; -0.3750];
+class_liftingBdy(2).tetherAttchPt = [21.2500; 0; -0.3750];
+class_liftingBdy(3).tetherAttchPt = [-2.5000; 20.0000; -0.3750];
+
+class_liftingBdy = reshape(class_liftingBdy,1,[]);
+
 % setpoints
 altitudeSP = 200;
 pitchSP = 2*pi/180;
@@ -29,18 +35,16 @@ ini_OwB = [0; 0; 0];
 %% turbine parameters
 % turbine struct
 turbine(1).Rturb_cm = [2.5000; -20.2500; 0];
-turbine(1).Cp = 0.5;
-turbine(1).Cd = 0.8;
-turbine(1).dia = 8.7;
+turbine(1).powerCoeff = 0.5;
+turbine(1).dragCoeff = 0.8;
+turbine(1).diameter =  8.7;
 
 turbine(2).Rturb_cm = [2.5000; 20.2500; 0];
-turbine(2).Cp = 0.5;
-turbine(2).Cd = 0.8;
-turbine(2).dia = 8.7;
+turbine(2).powerCoeff = 0.5;
+turbine(2).dragCoeff = 0.8;
+turbine(2).diameter =  8.7;
 
 %% tether parameters
-class_thr(1).R1_g = [-2.5000; -20.0000; 0.3750];
-class_thr(1).Rn_cm = [-2.5000; -20.0000; -0.3750];
 class_thr(1).numNodes = 4;
 class_thr(1).diameter = 0.055;
 class_thr(1).youngsModulus = 3.8e9;
@@ -51,8 +55,6 @@ class_thr(1).vehicleMass = 8.9360e+04;
 class_thr(1).ini_R1_o = [-2.5000; -20.0000; 0.3750];
 class_thr(1).ini_Rn_o = [-2.5271; -20.0000; 199.9325];
 
-class_thr(2).R1_g = [21.2500; 0; 0.3750];
-class_thr(2).Rn_cm = [21.2500; 0; -0.3750];
 class_thr(2).numNodes = 4;
 class_thr(2).diameter = 0.076;
 class_thr(2).youngsModulus = 3.8e9;
@@ -63,8 +65,6 @@ class_thr(2).vehicleMass = 8.9360e+04;
 class_thr(2).ini_R1_o = [21.2500; 0; 0.3750];
 class_thr(2).ini_Rn_o = [21.0459; 0; 197.0381];
 
-class_thr(3).R1_g = [-2.5000; 20.0000; 0.3750];
-class_thr(3).Rn_cm = [-2.5000; 20.0000; -0.3750];
 class_thr(3).numNodes = 4;
 class_thr(3).diameter = 0.055;
 class_thr(3).youngsModulus = 3.8e9;
@@ -105,6 +105,11 @@ c_damp = 10;
 ini_platform_ang = 0;
 ini_platform_vel = 0;
 
+class_gndStn(1).tetherAttchPt = [-2.5000; -20.0000; 0.3750];
+class_gndStn(2).tetherAttchPt = [21.2500; 0; 0.3750];
+class_gndStn(3).tetherAttchPt = [-2.5000; 20.0000; 0.3750];
+
+class_gndStn = reshape(class_gndStn,1,[]);
 
 %% avl parameters
 load('dsgnTest_1_lookupTables');
