eae62260c2c71a5597e4464b9beaf88d358914a4
Ayaz <asiddiq2@ncsu.edu>
2019-06-21 20:09:48 -0400

    changed turbine implementation from for iterator to for each loops. -AS

--- units/applicationSpecific/simpleDragTurbine/simpleDragTurbine_ts.m
+++ units/applicationSpecific/simpleDragTurbine/simpleDragTurbine_ts.m
@@ -5,14 +5,16 @@ format compact
 %% start
 % turbine struct
 turbine(1).Rturb_cm = [0;10;0];
-turbine(1).Cp = 0.5;
-turbine(1).Cd = 0.8;
-turbine(1).dia = 1;
+turbine(1).powerCoeff = 0.5;
+turbine(1).dragCoeff = 0.8;
+turbine(1).diameter = 1;
 
 turbine(2).Rturb_cm = [0;10;0];
-turbine(2).Cp = 0.5;
-turbine(2).Cd = 0.8;
-turbine(2).dia = 1;
+turbine(2).powerCoeff = 0.5;
+turbine(2).dragCoeff = 0.8;
+turbine(2).diameter = 1;
+
+turbine = reshape(turbine,1,[]);
 
 % other params
 V_flow = [1;0;0];
@@ -21,7 +23,7 @@ euler = [0;0;0];
 
 rho_fluid = 1;
 
-sim('simpleDragTurbine_th')
+% sim('simpleDragTurbine_th')
 
 
 
