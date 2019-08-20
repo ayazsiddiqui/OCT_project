a115a2bc00e72d54f2ea8a247a3be1286d8fd3d2
Ayaz <asiddiq2@ncsu.edu>
2019-08-19 20:58:54 -0400

    first pass at gaussian process classdef is a success

--- classes/+OPT/@gaussianProcess/gaussianProcess.m
+++ classes/+OPT/@gaussianProcess/gaussianProcess.m
@@ -1,44 +1,59 @@
-classdef gaussianProcess
+classdef gaussianProcess < dynamicprops
     %@GAUSSIANPROCESS Summary of this class goes here
     %   Detailed explanation goes here
     
-    properties 
+    properties (SetAccess = public)
         noInputs
-        kernel
-        hyperParameters
+        kernelName
     end
     
+    
     methods
-        %% contructor
+        % contructor
         
-        %         function obj = gaussianProcess(inputArg1,inputArg2)
-        %             %@GAUSSIANPROCESS Construct an instance of this class
-        %             %   Detailed explanation goes here
-        %             obj.Property1 = inputArg1 + inputArg2;
-        %         end
+        %                 function obj = gaussianProcess(obj)
+        %                     %@GAUSSIANPROCESS Construct an instance of this class
+        %                     %   Detailed explanation goes here
+        %                     obj.noInputs = [];
+        %                     obj.kernel = [];
+        %                     obj.hyperParameters = [];
+        %
+        %                 end
         
         %% setters
         % set number of design variables
-        function obj = setNoInputs(obj,val)
-            obj.noInputs = val;
-        end
-        
-        % set kernel
-        function obj = setKernel(obj,val)
-            if ischar(val)
-                obj.kernel = val;
+        function obj = set.noInputs(obj,val)
+            if numel(val)== 1
+                obj.noInputs = val;
             else
-                error('Kernel should be a character string')
+                error('Specify scalar value for number of inputs')
             end
         end
         
-        % set hyperParameters
-        function obj = setHyperParameters(obj,val)
-            obj.hyperParameters = val;
+        % set kernel
+        function obj = set.kernelName(obj,val)
+            if ischar(val)
+                obj.kernelName = val;
+            else
+                error('Kernel name should be a character string')
+            end
+        end
+        
+        % get kernel
+        function obj = getkernel(obj)
+            
+            obj.addprop('kernel');
+            
+            switch obj.kernelName
+                case 'squaredExponential'
+                    obj.kernel = OPT.squaredExponential;
+            end
+            
+            obj.kernel.noInputs = obj.noInputs;
         end
         
         %% other methods
-        % objective function (Ideally would be an input)
+        % objective function
         function val = objectiveFunction(obj,designPts)
             X = designPts;
             if size(designPts,1) ~= obj.noInputs
@@ -48,67 +63,81 @@ classdef gaussianProcess
             val = reshape(val,[],1);
         end
         
-        % build covariance mat
-        function covMat = buildCovarianceMatrix(obj,dsgnSet1,dsgnSet2,varargin)
-            
-            % function that builds covariance matrix between design sets
-            % inputs datapoints, covariance amplitude, noise variance and length scale
-            
-            % length scale quantifies the relevance of the components in the input
-            % vector for predicting the outputs
-            
-            switch obj.kernel
-                case 'squaredExponential'
-                    % squared exponential
-                    cov = @(x1,x2,covAmp,noiseVar,lengthScl)...
-                        covAmp*(exp(-0.5*((x1-x2)'*(eye(numel(lengthScl))./(lengthScl.^2))*(x1-x2))));
-            end
-            % parse input
-            p = inputParser;
-            addRequired(p,'dsgnSet1',@isnumeric);
-            addRequired(p,'dsgnSet2',@isnumeric);
-            addParameter(p,'covAmplitude',0,@isnumeric);
-            addParameter(p,'noiseVariance',0,@isnumeric);
-            addParameter(p,'lengthScale',1,@isnumeric);
-            
-            parse(p,dsgnSet1,dsgnSet2,varargin{:});
-            
-            if size(p.Results.lengthScale,1) ~= size(p.Results.dsgnSet1,1) || ...
-                    size(p.Results.lengthScale,1) ~= size(p.Results.dsgnSet2,1)
-                error('Number of elements in length scale not equal to number of input variables')
-            end
-            
-            % convariance matrix size
-            covMatSize = [size(dsgnSet1,2), size(dsgnSet2,2)];
-            covMat = zeros(covMatSize);
-            
-            if isequal(dsgnSet1,dsgnSet2)
-                for ii = 1:covMatSize(1)
-                    for jj = ii:covMatSize(2)
-                        covMat(ii,jj) = cov(dsgnSet1(:,ii),dsgnSet2(:,jj),...
-                            p.Results.covAmplitude,p.Results.noiseVariance,p.Results.lengthScale);
-                        
-                    end
-                end
-                covMat = covMat + covMat' - eye(length(covMat)).*diag(covMat - p.Results.noiseVariance);
-                
-            else
-                for ii = 1:covMatSize(1)
-                    for jj = 1:covMatSize(2)
-                        covMat(ii,jj) = cov(dsgnSet1(:,ii),dsgnSet2(:,jj),...
-                            p.Results.covAmplitude,p.Results.noiseVariance,p.Results.lengthScale);
-                        %             if ii == jj
-                        %                 covMat(ii,jj) = covMat(ii,jj) + p.Results.noiseVariance;
-                        %             end
-                        
-                    end
-                end
-            end
-            
-            
+        % build covariance matrix baed on kernel
+        function val = buildCovarianceMatrix(obj,dsgnSet1,dsgnSet2)
+            val = obj.kernel.buildCovarianceMatrix(dsgnSet1,dsgnSet2);
             
         end
+        
+        % calculate log likelihood
+        function val = calcLogLikelihood(obj,dsgnSet,varargin)
+            
+            switch obj.kernelName
+                case 'squaredExponential'
+                    p = inputParser;
+                    addParameter(p,'covarianceAmp',0,@isnumeric);
+                    addParameter(p,'noiseVariance',0,@isnumeric);
+                    addParameter(p,'lengthScale',1,@isnumeric);
+                    
+                    parse(p,varargin{:})
+                    
+                    obj.kernel.covarianceAmp = p.Results.covarianceAmp;
+                    obj.kernel.noiseVariance = p.Results.noiseVariance;
+                    obj.kernel.lengthScale = p.Results.lengthScale;
+            end
+            
+            Kmat = obj.buildCovarianceMatrix(dsgnSet,dsgnSet);
+            
+            y = obj.objectiveFunction(dsgnSet);
+            
+            val = -1*(-0.5*(y'/Kmat*y) - 0.5*log(det(Kmat)));
+        end
+        
+        % optimize hyper parameters
+        function val = optimizeHyperParameters(obj,dsgnSet,initialGuess)
+            
+            A = []; b = [];
+            Aeq = []; beq = [];
+            
+            lb = [0,0,zeros(1,obj.noInputs)];
+            ub = [1e3*[1,1],1e3*ones(1,obj.noInputs)];
+            
+            switch obj.kernelName
+                case 'squaredExponential'
+                    val = fmincon(@(hyper) ...
+                        obj.calcLogLikelihood(dsgnSet,...
+                        'covarianceAmp',hyper(1),'noiseVariance',hyper(2),...
+                        'lengthScale',hyper(3:end)),...
+                        initialGuess,A,b,Aeq,beq,lb,ub);
+                    
+            end
+        end
+        
+        % calculate predictive mean and variance
+        function [predMean,predVar] = calcPredictiveMeanAndVariance(obj,postDsgn,trainDsgn,trainFval)
+            
+            CovMatVecTranspose = obj.buildCovarianceMatrix(trainDsgn,postDsgn);
+            testCovMat = obj.buildCovarianceMatrix(trainDsgn,trainDsgn);
+            
+            % mean and variance
+            nPost = size(postDsgn,2);
+            
+            muD = NaN(nPost,1);
+            Var = NaN(nPost,1);
+            for ii = 1:nPost
+                muD(ii,1) = (CovMatVecTranspose(:,ii)'/testCovMat)*trainFval;
+                Var(ii,1) = obj.buildCovarianceMatrix(postDsgn(:,ii),postDsgn(:,ii))...
+                    - (CovMatVecTranspose(:,ii)'/testCovMat)*CovMatVecTranspose(:,ii);
+            end
+            
+            predMean = muD;
+            predVar = Var;
+            
+        end
+        
     end
     
 end
+
+
 
--- development/dummy.m
+++ development/dummy.m
@@ -6,33 +6,59 @@ clc
 format compact
 close all
 
-testDsgns = linspace(0,5,10);
-testDsgns = [testDsgns;testDsgns];
-
-covMat = buildCovMat(testDsgns,testDsgns,'covAmplitude',0.2,'noiseVariance',0,'lengthScale',[1;1]);
-
-
-objF = @(X) (0.25*X(1,:).*sin(2*X(1,:)).*exp(-0.5*X(1,:)) - 0.125*X(1,:).^2.*exp(-X(1,:)) + 0.12)';
-
-x = 1;
-% op = calcLogLogLikelihood(objF(testDsgns),testDsgns,[0.2,0,1]);
+rng('default');
+rng(4);
 
 %% test class
 gp = OPT.gaussianProcess;
 
-gp.setNoInputs(2);
-gp.setKernel('squaredExponential');
+gp.noInputs = 2;
+gp.kernelName = 'squaredExponential';
 
-nSamp = 4;
+nSamp = 20;
 xMin = -5; xMax = 5;
 x1 = ((xMax-xMin).*rand(1,nSamp) + xMin);
 x2 = ((xMax-xMin).*rand(1,nSamp) + xMin);
 testDsgns = [x1;x2];
 
-gp.objectiveFunction(testDsgns)
+trainFval = gp.objectiveFunction(testDsgns);
 
-Kmat = gp.buildCovarianceMatrix(testDsgns,testDsgns,'covAmplitude',0.5,'noiseVariance',0,'lengthScale',[1;1]);
+gp.getkernel
 
+gp.kernel.covarianceAmp = 1;
+gp.kernel.noiseVariance = 0;
+gp.kernel.lengthScale = [1;1];
 
+Kmat = gp.buildCovarianceMatrix(testDsgns,testDsgns);
 
+logLike = gp.calcLogLikelihood(testDsgns,'covarianceAmp',1,'noiseVariance',0,'lengthScale',[6;1]);
 
+initialGuess = 1*rand(2+gp.noInputs,1);
+
+val = gp.optimizeHyperParameters(testDsgns,initialGuess);
+
+gp.kernel.covarianceAmp = val(1);
+gp.kernel.noiseVariance = val(2);
+gp.kernel.lengthScale = val(3:end);
+
+%%
+nPost = 500;
+x1Post = ((xMax-xMin).*rand(1,nPost) + xMin);
+x2Post = ((xMax-xMin).*rand(1,nPost) + xMin);
+postDsgns = [x1Post;x2Post];
+
+[predMean,predVar] = gp.calcPredictiveMeanAndVariance(postDsgns,testDsgns,trainFval);
+
+%% plot results
+figure(1)
+scatter3(postDsgns(1,:),postDsgns(2,:),gp.objectiveFunction(postDsgns),'k');
+hold on
+scatter3(testDsgns(1,:),testDsgns(2,:),gp.objectiveFunction(testDsgns)','+b');
+
+% estimations
+scatter3(postDsgns(1,:),postDsgns(2,:),predMean,'r');
+grid on
+% scatter3(postDsgns,upperLimitMean,'-.m')
+% scatter3(postDsgns,lowerLimitMean,'-.m')
+
+legend('objF','Sampled points','Estimated mean')
